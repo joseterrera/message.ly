af3d233406b31ce8899a6a9acdaefb88fafc644f
Jose Terrera <joseterrera@yahoo.com>
2020-11-07 20:18:06 -0800

    finishing routes messages

--- routes/messages.js
+++ routes/messages.js
@@ -20,11 +20,15 @@ const ExpresError = require("../expressError");
 router.get(":/id", ensureLoggedIn, async function(req, res, next) {
   try {
     let username = req.user.username;
-    let msg 
+    let msg = await Message.get(req.params.id);
+    if( msg.to_user.username !== username && msg.from_user,username !== username) {
+      throw new ExpresError("cannot read this message", 404)
+    }
+    return res.json({message: msg})
   } catch (err){
     return next(err);
   }
-})
+});
 
 
 /** POST / - post message.
@@ -33,6 +37,20 @@ router.get(":/id", ensureLoggedIn, async function(req, res, next) {
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+  try {
+    let msg = await Message.create( {
+      from_username: req.user.username,
+      to_username: req.body.to_username,
+      body: req.body.body
+    });
+
+  return res.json({ message: msg})
+
+  } catch (err) {
+    return next(err)
+  }
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -43,3 +61,20 @@ router.get(":/id", ensureLoggedIn, async function(req, res, next) {
  *
  **/
 
+ router.post("/:id/read", ensureLoggedIn, async function(req, res, next) {
+   try {
+     let username = req.user.username;
+     let msg = await Message.get(req.params.id);
+
+     if(msg.to_user.username !== username) {
+       throw new ExpresError("Cannot set this message to read", 401)
+     }
+     let message = await Message.markRead(req.params.id);
+     return res.json({messge});
+   }
+   catch(err) {
+     return next(err);
+   }
+ });
+
+module.exports = router;
